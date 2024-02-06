c24aa8f9e27c4984b6cc73b8da3d37e26f356376
Aadesh Motghare <109069284+AadeshMotghare@users.noreply.github.com>
2024-02-06 15:17:28 +0530

    Aadesh

--- src/components/PreLoader.jsx
+++ src/components/PreLoader.jsx
@@ -11,13 +11,8 @@ const PreLoader = () => {
     }, []);
     return ( 
     <>
-<<<<<<< Updated upstream
-        <div className="preloader">
-            <div className='texts-container '>
-=======
         <div className="preloader no-scrollbar">
             <div className='space-x-3 texts-container '>
->>>>>>> Stashed changes
                 <span > FIND  </span> 
                 <span > THE </span> 
                 <span > PERFECT  </span> 
