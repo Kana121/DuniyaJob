2aadf8c8a23de964f19a6b27fa4fcdfb7bbad985
Janhvi Pardeshi <janhvip.etc20@sbjit.edu.in>
2024-02-09 16:48:43 +0530

    janhvi

--- src/pages/_root/innerpages/PostJobs.jsx
+++ src/pages/_root/innerpages/PostJobs.jsx
@@ -1,13 +1,32 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import Header from '../../../components/innerNavbar/Header'
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import job from '../../../assets/post_job.png'
 
 const PostJobs = () => {
   return (
+    <>
     <div>
-      <Box >
-        hello
-      </Box>
+      <Header/>
     </div>
+    <div className=' flex mt-28'>
+    <React.Fragment>
+      <CssBaseline />
+      <Container >
+        <Box sx={{ bgcolor: '#cfe8fc', height: '50vh', display: 'flex',}} >
+
+          <Box alignSelf={'flex-end'} sx={{width:'50%', height: '100%',}}>
+            <img src={job} className=' flex object-cover'/>
+          </Box>
+
+        </Box>
+        
+      </Container>
+    </React.Fragment>
+    </div>
+    </>
     
   )
 }
