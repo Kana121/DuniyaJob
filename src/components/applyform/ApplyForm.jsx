7389c09449ddb2e5fe0df883e654177192df843a
Anuj Gajbhiye <117268566+anuj03gajbhiye@users.noreply.github.com>
2024-02-13 10:04:32 +0530

    Anuj

--- src/components/applyform/ApplyForm.jsx
+++ src/components/applyform/ApplyForm.jsx
@@ -5,6 +5,7 @@ const ApplyForm = () => {
   return (
         <Box alignSelf={'center'} width={'60%'} height={'150px'} sx={{border:'solid 1px', borderRadius:'20px',background:'white',mt:'20px'}}>
 
+
         </Box>
     )
 }
