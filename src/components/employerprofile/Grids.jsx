09913668cc19502ff0de33c054d9e5af13ccd102
Janhvi Pardeshi <janhvip.etc20@sbjit.edu.in>
2024-02-14 14:41:41 +0530

    janhvi

--- src/components/employerprofile/Grids.jsx
+++ src/components/employerprofile/Grids.jsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import RadioGroup from '@mui/material/RadioGroup';
-import Radio from '@mui/material/Radio';
 import Paper from '@mui/material/Paper';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
 // import HighlightedCode from 'docs/src/modules/components/HighlightedCode';
+import gridData from './gridtext.json';
 
-export default function Grids() {
+export default function SpacingGrid() {
   const [spacing, setSpacing] = React.useState(2);
 
   const handleChange = (event) => {
@@ -23,39 +24,31 @@ export default function Grids() {
     <Grid sx={{ flexGrow: 1 }} container spacing={2}>
       <Grid item xs={12}>
         <Grid container justifyContent="center" spacing={spacing}>
-          
+          {gridData.map((gridData, index) => (
+            <Grid key={index} item>
+              <Paper variant='elivation'
+                sx={{
+                  height: 350,
+                  width: 350,
+                  borderRadius: '7vh',
+                  opacity: '80%',
+                  ":hover":{boxShadow: '15px', opacity: '100%'},
+                  backgroundColor: (theme) =>
+                    theme.palette.mode === 'dark' ? '#1A2027' : 'lightblue',
+                  
+                }}
+              >
+                <h1 className=' text-center text-2xl font-extrabold text-black pt-20'>{gridData.name}</h1>
+                <p className=' text-black text-center pt-7 pl-6 pr-6'>{gridData.description}</p>
+              </Paper>
+            </Grid>
+          ))}
         </Grid>
       </Grid>
-      {/* <Grid item xs={12}> */}
-        {/* <Paper sx={{ p: 2 }}> */}
-          {/* <Grid container> */}
-            {/* <Grid item> */}
-              
-                {/*
-                <FormControl component="fieldset">
-                <FormLabel component="legend">spacing</FormLabel><RadioGroup
-                  name="spacing"
-                  aria-label="spacing"
-                  value={spacing.toString()}
-                  onChange={handleChange}
-                  row
-                >
-                  {[0, 0.5, 1, 2, 3, 4, 8, 12].map((value) => (
-                    <FormControlLabel
-                      key={value}
-                      value={value.toString()}
-                      control={<Radio />}
-                      label={value.toString()}
-                    />
-                  ))}
-                </RadioGroup>
-                </FormControl>*/}
-              
-            {/* </Grid> */}
-          {/* </Grid> */}
-        {/* </Paper> */}
+      <Grid item xs={12}>
+        
         {/* <HighlightedCode code={jsx} language="jsx" /> */}
-      {/* </Grid> */}
+      </Grid>
     </Grid>
   );
 }
